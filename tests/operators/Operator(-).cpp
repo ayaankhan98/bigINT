7c9819bed98743d71cad38b971d1102b1430522e
Alok Tripathi <70872230+Tripathi-Alok@users.noreply.github.com>
2020-10-04 23:26:06 +0530

    Create Operator(-).cpp

---
+++ tests/operators/Operator(-).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class largeInt {
+   private:
+    long int real;
+   public:
+    largeInt() : real(0) {}
+
+    void input() {
+        cin >> real;
+    }
+
+    largeInt operator + (const largeInt& obj) {
+        largeInt temp;
+        temp.real = real - obj.real;
+        return temp;
+    }
+
+    void output() 
+    {
+        cout<< real <<endl;
+    }
+};
+
+int main() {
+    largeInt a, b, result;
+
+    a.input();
+    b.input();
+    result = a + b;
+    result.output();
+
+    return 0;
+}
