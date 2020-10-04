e6f77c409b159f583fe40b21f8e3488b3a766b11
Chidambar Joshi <54184017+Chidu2000@users.noreply.github.com>
2020-10-04 10:51:42 +0530

    Create Chidu2000.cpp
    wrote some description about big integer library in c++

---
+++ Chidu2000.cpp
@@ -0,0 +1,7 @@
+Big integer library in C++ is very robust for handling big size numbers in c++.
+sqrt() gives squareroot of a number instead of involving much calculations.
+pow() gives the any power of a number .
+max(a,b) gives the maximum value among variables a,b.
+min(a,b) gives the minimum value among variables a,b.
+instead of writing bigint we now write int
+Using biginteger , we can calculate factorial of 1000 easily ,without much calculations.
