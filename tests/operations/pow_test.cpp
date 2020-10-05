d1322df02020ac1e88150656ef65429f4e214a1b
anu231bhav <anu231bhav@gmail.com>
2020-10-05 18:56:41 +0530

    Added pow(a,b)=a^b function to find power.

---
+++ tests/operations/pow_test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cassert>
+
+#include "../../src/bigint.hpp"
+
+int main() {
+  freopen("input.txt", "r", stdin);
+
+  
+  int a, b;
+  std::cin >> a,b;
+  
+  libbig::largeInt * num=pow(a,b);
+
+  assert(num->number == "81");
+  
+  
+  std::cin >> a,b;
+  
+  libbig::largeInt * num=pow(a,b);
+
+  assert(num->number == "262144");
+  
+  
+  std::cin >> a,b;
+  
+  libbig::largeInt * num=pow(a,b);
+
+  assert(num->number == "1977326743");
+
+ 
+  return 0;
+}
