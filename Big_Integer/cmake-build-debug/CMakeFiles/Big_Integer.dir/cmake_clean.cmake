0b5dcdd05f2a8a340a84b93f7d911bf05bb61d46
punitsharma077 <punitsharma077@gmail.com>
2020-10-06 15:57:25 +0530

    Add files via upload
    My implementation which i did during my work.

---
+++ Big_Integer/cmake-build-debug/CMakeFiles/Big_Integer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Big_Integer"
+  "Big_Integer.pdb"
+  "CMakeFiles/Big_Integer.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Big_Integer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Big_Integer/cmake-build-debug/CMakeFiles/clion-log.c
@@ -0,0 +1,4 @@
+/home/punit/clion-2020.2.1/bin/cmake/linux/bin/cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" "/home/punit/Punit Folder/CLionProjects/Big_Integer"
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /home/punit/Punit Folder/CLionProjects/Big_Integer/cmake-build-debug
