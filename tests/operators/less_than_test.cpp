48f7f1fd5268de541a3510dff0be3a64625c1ca6
Yash Soni <yashsoni501@gmail.com>
2020-10-07 23:41:51 +0200

    Corrected typos in tester

--- tests/operators/less_than_test.cpp
+++ tests/operators/less_than_test.cpp
@@ -9,16 +9,14 @@ int main()
 
     std::random_device rd; // obtain a random number from hardware
     std::mt19937 gen(rd()); // seed the generator
-    std::uniform_int_distribution<> distr(-1e18, 1e18); // define the range
+    std::uniform_int_distribution<> distr(-2147483648, 2147483647); // define the range
 
     for(int i=0;i<100;i++)
     {
-        long long i1 = distr(gen);
-        long long i2 = distr(gen);
-        
+        int i1 = distr(gen);
+        int i2 = distr(gen);
         a = i1;
         b = i2;
-
         assert((a<b) == (i1<i2));
     }
 }
