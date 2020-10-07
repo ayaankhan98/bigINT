59449bf6a703712478b3e05b577c226cee99a222
anu231bhav <anu231bhav@gmail.com>
2020-10-07 15:01:29 +0530

    Added pow(a,b)=a^b function to find power.

--- tests/operations/pow_test.cpp
+++ tests/operations/pow_test.cpp
@@ -6,12 +6,12 @@ int main() {
   libbig::largeInt ans("232");
 
   libbig::largeInt * num=ans.pow(3,4);
-  assert(num->nugetnumber() == std::string("81"));
+  assert(num->getnumber() == std::string("81"));
 
-  num=libbig::ans.pow(4,9);
+  num=ans.pow(4,9);
   assert(num->getnumber() == std::string("262144"));
   
-  num=libbig::ans.pow(7,11);
+  num=ans.pow(7,11);
   assert(num->getnumber() == std::string("1977326743"));
   
   return 0;
