842e4ca7ffa5498214e6e6ac0cebf27765f0c0b3
Ryuzaki <ufrshubham@gmail.com>
2020-10-10 14:30:20 +0530

    Added try-catch

--- tests/operators/ostream_test.cpp
+++ tests/operators/ostream_test.cpp
@@ -7,24 +7,31 @@
 
 int main()
 {
-    libbig::largeInt a;
-    std::stringstream buffer;
-    std::ifstream f("input.txt");
-    std::string line;
+    std::ifstream f;
 
-    if (f.is_open())
+    try
     {
+        f.open("input.txt");
+        std::string line;
+
         while (std::getline(f, line, ','))
         {
+            libbig::largeInt a;
+            std::stringstream buffer;
+
             a = libbig::largeInt(line);
             buffer << a;
+
             assert(buffer.str().compare(std::string(line)) == 0);
-            buffer.str(std::string());
         }
         f.close();
-    } else {
-        throw std::runtime_error("Couldn't open input.txt");
     }
-
+    catch(const std::ifstream& e)
+    {
+        // Failure to open file stream essentially means 
+        // something is wrong somewhere. So assert.
+        f.close();
+        assert(false);
+    }
     return 0;
 }
